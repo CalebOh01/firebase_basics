380bd5de9c8c50bf7b82ce8b828d4111da762349
Caleb Oh <caleboh01@gmail.com>
2021-06-07 01:13:15 -0400

    first db usage

--- public/app.js
+++ public/app.js
@@ -1,7 +1,21 @@
 document.addEventListener("DOMContentLodaded", event => {
      const app = firebase.app();
+
      
 })
+
+function getPost() {
+     const db = firebase.firestore()
+
+     const myPost = db.collection('posts').doc('firstpost')
+
+     myPost.get()
+          .then(doc => {
+               const data = doc.data()
+               document.write(data.title)
+               document.write(data.views)
+          })
+}
 
 function googleLogin() {
      const provider = new firebase.auth.GoogleAuthProvider()
